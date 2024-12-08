04c7248b4ff039e0f3d66d7a7fccdbe4686c2cfd
Vadim Sapovsky <vadym.sapovskyi@wework.com>
2024-12-08 16:45:50 +0200

    add solution for 2-module 1-task

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -5,13 +5,45 @@ import { Task } from "./task.model";
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+    return task;
+  }
 
-  createTask(task: Task): Task {}
+  createTask(task: Task): Task {
+    const newTask = { ...task, id: Math.random().toString() };
+    this.tasks.push(newTask);
 
-  updateTask(id: string, update: Task): Task {}
+    return newTask;
+  }
 
-  deleteTask(id: string): Task {}
+  updateTask(id: string, update: Task): Task {
+    const task = this.getTaskById(id);
+    const updatedTask = { ...task, ...update };
+
+    this.tasks = this.tasks.map((task) =>
+      task.id === id ? updatedTask : task
+    );
+
+    return updatedTask;
+  }
+
+  deleteTask(id: string): Task {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+
+    if (taskIndex === -1) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    const [deletedTask] = this.tasks.splice(taskIndex, 1);
+    
+    return deletedTask;
+  }
 }
